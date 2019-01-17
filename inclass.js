d2b864dae7f85f4d3bc83f9d133c303ba28f1941
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-01-17 09:29:32 -0700

    first commit.

---
+++ inclass.js
@@ -0,0 +1,5 @@
+function addNum() {
+    let n1 = document.getElementById("num1").innerHTML;
+    let n2 = document.getElementById("num2").innerHTML;
+    document.getElementById("result").value = num1 + num2;
+}
